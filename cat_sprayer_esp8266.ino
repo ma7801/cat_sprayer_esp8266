c2eb65460bd9d24ac985fe4272db64be045f4c47
ma7801 <ma7801@gmail.com>
2019-04-13 20:24:05 -0700

    Update cat_sprayer_esp8266.ino

--- cat_sprayer_esp8266.ino
+++ cat_sprayer_esp8266.ino
@@ -327,7 +327,6 @@ void t_spray() {
   }
 }
 
-
 // If sprayer button pressed on Blynk
 BLYNK_WRITE(V4) {
   blynkSprayButton = (bool)param.asInt();
@@ -348,7 +347,6 @@ BLYNK_WRITE(V4) {
   }
 }
 
-
 void spray() {
   Serial.println("Spray!!!");
   Blynk.virtualWrite(V2, blynkHIGH);
@@ -356,136 +354,7 @@ void spray() {
 }
 
 void loop() {
-
   Blynk.run();
   timer.run();
  
-  
-  // If disabled
-  /*
-  if(remainingDisabledIterations > 0) {
-    
-    #ifdef DEBUG
-    Serial.println("Sleeping for 8 seconds...");
-    delay(SERIAL_DELAY);
-    #endif
-
-    //Delay to improve button operation
-    delay(buttonDelay);
-    
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-
-    #ifdef DEBUG
-    Serial.println("Waking up!");
-    delay(SERIAL_DELAY);
-    #endif
-    
-    remainingDisabledIterations--;
-
-    // If last iteration elapsed:
-    if(remainingDisabledIterations == 0) {
-      // If there is a second disable interval (i.e. user hit button twice)
-      if(secondInterval) {
-        secondInterval = false;
-    
-        // Turn off LED2
-        digitalWrite(LED2Pin, LOW);
-
-        // Indicate that only one LED is on now
-        LEDOnCount = 1;
-    
-        // Reset the iteration count to disabledIterationsInterval
-        remainingDisabledIterations = disabledIterationInterval;
-    
-      }
-      
-      // Otherwise, disabled period totally elapsed
-      else {
-        // Turn off LED1
-        digitalWrite(LED1Pin, LOW);
-
-        // Indicate now LEDs on
-        LEDOnCount = 0;
-
-        // Set Ok to power down flag
-        okToIdle = true;
-      }
-    } 
-  }
-*/
-  
-
-/*
-  // If motion detected
-  if(pir_triggered) {
-
-    // Reset flag
-    pir_triggered = false;
-    
-    // Check if in disabled state; if so, don't spray, just repeat the loop()
-    if (remainingDisabledIterations > 0) return;
-
-    // Activate sprayer
-    digitalWrite(sprayerPin, HIGH);
-    
-    #ifdef DEBUG
-    Serial.println("Spray!");
-    #endif
-
-    #ifdef DEBUG
-    digitalWrite(13, HIGH);
-    #endif
-    
-    delay(sprayDuration);
-
-    #ifdef DEBUG
-    digitalWrite(13, LOW);
-    #endif
-    
-    digitalWrite(sprayerPin, LOW);
-  }
-
-  // Enter a low power idle until interrupt activated
-  if(okToIdle) {
-    #ifdef DEBUG
-    Serial.print("button_pressed=");
-    Serial.println(button_pressed);
-    Serial.print("millis()=");
-    Serial.println(millis());
-    Serial.println("Going to idle state...");
-    delay(SERIAL_DELAY);
-    #endif
-
-    // Delay to improve button operation
-    delay(350);
-    
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-
-    #ifdef DEBUG
-    Serial.println("Coming out of idle state...");
-    Serial.print("button_pressed=");
-    Serial.println(button_pressed);
-    Serial.print("millis()=");
-    Serial.println(millis());
-    delay(SERIAL_DELAY);
-    #endif
-  } 
-
-  */
 }
-
-/*
-void pirOnISR() {
-  #ifdef DEBUG
-  Serial.println("PIR trigger!");
-  delay(SERIAL_DELAY);
-  #endif
-
-  pir_triggered = true;
-
-  // Bug fix for button somehow getting triggered when either PIR is triggered or spray occurs:
-  delay(350);
-  button_pressed = false;
-}
-
-*/
